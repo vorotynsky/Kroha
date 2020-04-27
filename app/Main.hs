b8f02a619ef9e55baabf918deea1edd9e794c38b
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2020-04-27 22:36:05 +0300

    feat: type check

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,11 @@ import           Control.Monad ((>=>))
 import           Funcs
 import           HLasm.Parser
 import           HLasm.Scope
+import           HLasm.Types
 
 parseAll :: String -> String
 parseAll = get . fmap (drawTree . fmap show) . pipeline
-    where pipeline = err "Parse error" parse >=> err "Scope error" (semantic)
+    where pipeline = err "Parse error" parse >=> err "Scope error" (semantic) >=> err "Type error" typeCheck
 
 main :: IO ()
 main = do
---
+++ src/HLasm/Types.hs
@@ -0,0 +1,49 @@
+-- Copyright (c) 2020 Vorotynsky Maxim
+
+module HLasm.Types where
+
+import           Data.Maybe
+import           Data.Tree
+import           HLasm.Ast
+import           HLasm.Scope
+
+typeSuit :: Type -> Type -> Bool
+typeSuit (Type lname _) (Type rname _) | (lname /= rname) = False
+typeSuit (Type _ l) (Type _ r) = (fromMaybe maxBound l) >= (fromMaybe maxBound r)
+
+registerSize :: RegisterName -> Int
+registerSize (n:"l")     | elem n "abcd" = 8
+registerSize (n:"h")     | elem n "abcd" = 8
+registerSize (n:"x")     | elem n "abcd" = 16
+registerSize ('e':n:"x") | elem n "abcd" = 32
+registerSize ('r':n:"x") | elem n "abcd" = 64
+registerSize _                           = 0
+
+getType :: HLElement -> Type
+getType (VariableDeclaration (Variable (_, t))) = t
+getType (VariableDeclaration (Register (_, r))) = Type "int" (Just (registerSize r))
+
+lookupType :: VariableName -> [VariableData] -> Maybe Type
+lookupType name = fmap getType . lookup name . fmap (\(VariableData x) -> x)
+
+literalType :: [VariableData] -> HLValue -> Maybe Type
+literalType _ (IntegerValue x) = Just $ Type "int" (Just $ size x)
+    where size = ceiling . (\x -> log (x + 1) / log 2) . toEnum
+literalType s (NameValue name) = lookupType name s
+literalType _ _ = undefined
+
+astCheck :: HLElement -> [VariableData] -> Bool
+astCheck (IfBranch (Just (Condition (left, _, righ)))) xs = fromMaybe False $
+    do leftType  <- literalType xs left
+       rightType <- literalType xs righ
+       Just $ typeSuit leftType rightType
+astCheck (Assigment left right) xs = fromMaybe False $
+    do leftType  <- lookupType left xs
+       rightType <- literalType xs right
+       Just $ typeSuit leftType rightType
+astCheck _ _ = True
+
+typeCheck :: SemanticTree -> Maybe SemanticTree
+typeCheck tree = traverse f tree
+    where f x@(elem, vars, _) = if astCheck elem vars then Just x else Nothing
+
