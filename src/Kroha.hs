aea873c48bfe35108dbde8f2fc829974615868db
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2020-08-29 23:52:03 +0300

    refactor: stack offset to stack range

--- src/Kroha.hs
+++ src/Kroha.hs
@@ -7,18 +7,16 @@ import           Data.Tree         (Tree (..), drawTree)
 import           Kroha.Ast         (FrameElement (..), selectorProg)
 import           Kroha.Parser      (parse)
 import           Kroha.Scope       (linkProgram, linksTree)
-import           Kroha.Stack       (stackFrames)
+import           Kroha.Stack       (stack)
 import           Kroha.Types       (resolve, typeCasts)
 
 
 kroha :: String -> Either String String
-kroha src = do (sizes, frames) <- fmap munzip compile
-               let tree = drawTree $ Node "program" ((fmap . fmap) show frames)
-               return (show sizes ++ "\n" ++ tree)
+kroha src = fmap (drawTree . fmap show) compile
     where compile = do
                     program <- first id   $ parse src
                     scopes  <- first show $ linkProgram program
                     let programTree = Node (Instructions []) (selectorProg (const $ Instructions []) id program)
                     types   <- first show $ resolve 16 . typeCasts $ mzip (linksTree program) scopes
-                    let stack = stackFrames 16 program
-                    return stack
+                    let stackR = stack 16 program
+                    return stackR
